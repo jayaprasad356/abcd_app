6c4739ead770d323fb94cfa2e0213da08488d479
tamil6300 <tamil632000@gmail.com>
2022-11-13 02:30:33 +0530

    Home fragment

--- app/src/main/java/com/app/abcdapp/HomeFragment.java
+++ app/src/main/java/com/app/abcdapp/HomeFragment.java
@@ -7,58 +7,57 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    EditText otp_textbox_one, otp_textbox_two, otp_textbox_three, otp_textbox_four,otp_textbox_five,otp_textbox_six,otp_textbox_seven,otp_textbox_eight,otp_textbox_nine,otp_textbox_ten;
+
+
+
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        otp_textbox_one = root.findViewById(R.id.otp_edit_box1);
+        otp_textbox_two = root.findViewById(R.id.otp_edit_box2);
+        otp_textbox_three = root.findViewById(R.id.otp_edit_box3);
+        otp_textbox_four = root.findViewById(R.id.otp_edit_box4);
+        otp_textbox_five = root.findViewById(R.id.otp_edit_box5);
+        otp_textbox_six = root.findViewById(R.id.otp_edit_box6);
+        otp_textbox_seven = root.findViewById(R.id.otp_edit_box7);
+        otp_textbox_eight = root.findViewById(R.id.otp_edit_box8);
+        otp_textbox_nine = root.findViewById(R.id.otp_edit_box9);
+        otp_textbox_ten = root.findViewById(R.id.otp_edit_box10);
+
+
+
+        EditText[] edit = {otp_textbox_one, otp_textbox_two, otp_textbox_three, otp_textbox_four};
+
+        otp_textbox_one.addTextChangedListener(new GenericTextWatcher(otp_textbox_one, edit));
+        otp_textbox_two.addTextChangedListener(new GenericTextWatcher(otp_textbox_two, edit));
+        otp_textbox_three.addTextChangedListener(new GenericTextWatcher(otp_textbox_three, edit));
+        otp_textbox_four.addTextChangedListener(new GenericTextWatcher(otp_textbox_four, edit));
+        otp_textbox_five.addTextChangedListener(new GenericTextWatcher(otp_textbox_five, edit));
+        otp_textbox_six.addTextChangedListener(new GenericTextWatcher(otp_textbox_six, edit));
+        otp_textbox_seven.addTextChangedListener(new GenericTextWatcher(otp_textbox_seven, edit));
+        otp_textbox_eight.addTextChangedListener(new GenericTextWatcher(otp_textbox_eight, edit));
+        otp_textbox_nine.addTextChangedListener(new GenericTextWatcher(otp_textbox_nine, edit));
+        otp_textbox_ten.addTextChangedListener(new GenericTextWatcher(otp_textbox_ten, edit));
+
+
+        return root;
     }
 }
